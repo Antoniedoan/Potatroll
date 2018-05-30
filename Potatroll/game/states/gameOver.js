ea5f0a60791028074f1b01e66a0fae2fef8c6ef0
thanhdoan <antoniedoan@gmail.com>
2018-05-30 12:19:36 +0800

    del outcome, added gameOver

---
+++ Potatroll/game/states/gameOver.js
@@ -0,0 +1,19 @@
+var GameOver = function(game){}
+
+GameOver.prototype = {
+  preload: function () {
+
+  },
+  create: function () {
+    game.stage.backgroundColor = 0xfff000;
+    // this.score_text = this.game.add.text(this.game.world.centerX, 325, this.game.score, { font: '32px Arial', fill: '#ffffff', align: 'center'});
+    // this.score_text.anchor.setTo(0.5, 0.5);
+    var replay = game.add.button(game.world.centerX, game.world.centerY, 'replay',this.update);
+    //var replayText = game.add.text();
+  },
+  update: function () {
+    if(this.game.input.activePointer.justPressed()) {
+      this.game.state.start('play');
+    }
+  }
+};
