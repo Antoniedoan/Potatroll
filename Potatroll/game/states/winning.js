1a7937d0142996897c2b620736478915c47fc3a4
Thanh <antoniedoan@gmail.com>
2018-05-30 12:15:21 +0800

    Add files via upload

---
+++ Potatroll/game/states/winning.js
@@ -0,0 +1,20 @@
+
+var Winning = function(game){}
+
+Winning.prototype = {
+  preload: function () {
+
+  },
+  create: function () {
+    game.stage.backgroundColor = 0xffff00;
+    // this.score_text = this.game.add.text(this.game.world.centerX, 325, this.game.score, { font: '32px Arial', fill: '#ffffff', align: 'center'});
+    // this.score_text.anchor.setTo(0.5, 0.5);
+    var replay = game.add.button(game.world.centerX, game.world.centerY, 'replay',this.update);
+    //var replayText = game.add.text();
+  },
+  update: function () {
+    if(this.game.input.activePointer.justPressed()) {
+      this.game.state.start('play');
+    }
+  }
+};
